6c15b8d6a89c1e377e3daa5e250f5059160aa5ec
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-01-14 11:43:23 -0300

    DevController Created

--- back-end/src/Controllers/DevController.js
+++ back-end/src/Controllers/DevController.js
@@ -1,7 +1,14 @@
+const axios = require('axios')
+const Dev = require('../Models/Dev')
+
 module.exports = {
   async store (request, response) {
     const { github_username, techs, latitude, longitude } = request.body
     
+    let dev = await Dev.findOne({ github_username })
+
+    if (dev) return response.json({ message: "Cannot create, this developer already exists!"})
+
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
     
     const { name = login, avatar_url, bio } = apiResponse.data
@@ -14,7 +21,7 @@ module.exports = {
     }
   
     // Short syntax, same propertie and value name can be simplified
-    const dev = await Dev.create({
+    dev = await Dev.create({
       github_username: github_username,
       name,
       avatar_url,
@@ -24,5 +31,10 @@ module.exports = {
     })
   
     return response.json({ dev })
+  },
+
+  async index (request, response) {
+    const devs = await Dev.find()
+    return response.json( devs )
   }
 }
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,6 +1,5 @@
 const { Router } = require('express')
-const axios = require('axios')
-const Dev = require('./models/Dev')
+const DevController = require('./Controllers/DevController')
 
 const routes = Router()
 
@@ -10,35 +9,8 @@ const routes = Router()
 // Route Params: request.params (Identificar um recurso na alteração ou remoção) ex: www.sample.com/users/1
 // Body: request.body (Dados para criação) 
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+routes.get('/', DevController.index)
 
-routes.post('/devs', async (request, response) => {
-	const { github_username, techs, latitude, longitude } = request.body
-	
-	const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
-	
-	const { name = login, avatar_url, bio } = apiResponse.data
-	
-	const techsArray = techs.split(',').map(tech => tech.trim())
-
-	const location = {
-		type: 'Point',
-		coordinates: [longitude, latitude]
-	}
-
-	// Short syntax, same propertie and value name can be simplified
-	const dev = await Dev.create({
-		github_username: github_username,
-		name,
-		avatar_url,
-		bio,
-		techs: techsArray,
-		location
-	})
-
-  return response.json({ dev })
-})
+routes.post('/devs', DevController.store)
 
 module.exports = routes
