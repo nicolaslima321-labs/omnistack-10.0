59dc50243bfa1bb08381668b6fc97ea862eeee94
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-01-15 10:40:53 -0300

    Understanding of state

--- web/src/App.js
+++ web/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Componentes: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
-// Propriedade: Atributo de uma tag JSX
-// Estado:
+// Propriedade: Informações que um componente PAI passa para um componente FILHO (Ex: Atributo de uma tag JSX)
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
 
-import Header from './Header';
 
 function App() {
+  
+  const [counter, setCounter] = useState(0)
+
+  function incrementCounter() {
+    setCounter(counter + 1)
+  }
+
   return (
-    <Header title="Teste"></Header>
+    // Fragment
+    <>
+    <h1>Contador: {counter}</h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
