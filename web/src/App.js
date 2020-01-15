d29dfffdb43cc4180b2c28ceee785a786e29b33c
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-01-15 10:32:08 -0300

    First steps at front-end, React understanding

---
+++ web/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Componentes: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Atributo de uma tag JSX
+// Estado:
+
+import Header from './Header';
+
+function App() {
+  return (
+    <Header title="Teste"></Header>
+  );
+}
+
+export default App;
