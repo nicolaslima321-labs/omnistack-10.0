3b219d98839072f24485bbd83612a03becc7a41b
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-01-16 17:48:43 -0300

    Connecting with back-end

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
-import { Text, Image, View, StyleSheet } from 'react-native'
+import { Text, TextInput, Image, View, StyleSheet, TouchableOpacity } from 'react-native'
 import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons'
+
+import api from '../services/api'
 
 export default function Main({ navigation }) {
+	const [devs, setDevs] = useState([])
 	const [currentRegion, setCurrentRegion] = useState(null)
 
 	useEffect(() => {
@@ -29,22 +33,72 @@ export default function Main({ navigation }) {
 		loadInitialPosition()
 	}, [])
 
+	async function loadDevs() {
+		const { latitude, longitude } = currentRegion
+
+		const response = await api.get('/search' , {
+			params: {
+				latitude,
+				longitude,
+				techs: 'PHP'
+			}
+		})
+
+		console.log('test')
+		console.log(response.status)
+
+		setDevs(response.data.devs)
+	}
+
+	function handleRegionChanged(region) {
+		console.log(region)
+		setCurrentRegion(region)
+	}
+
 	if (!currentRegion) return null
 
   return (
-		<MapView initialRegion={currentRegion} style={styles.map}>
-			<Marker coordinate={{ latitude: -23.2750818, longitude: -45.9367478 }}>
-				<Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/23743072?s=460&v=4' }} />
-				<Callout style={styles.callout} onPress={() => {
-					// Navegação para página do GitHub
-					navigation.navigate('Profile', { github_username: 'nicolaslima321' })
-				}}>
-					<Text style={styles.devName}>Nicolas Lima</Text>
-					<Text style={styles.devBio}>Web Developer, student of computer engineering at Universidade do Vale do Paraíba</Text>
-					<Text style={styles.devTechs}>PHP, Laravel, JavaScript, VueJS</Text>
-				</Callout>
-			</Marker>
-		</MapView>
+		<>
+			<MapView 
+				onRegionChangeComplete={handleRegionChanged}
+				initialRegion={currentRegion} 
+				style={styles.map}
+			>
+				{devs.map(dev => {
+					<Marker
+						key={dev._id}
+						coordinate={{ longitude: dev.location.coordinates[0], latitude: dev.location.coordinates[1] }}
+					>
+					<Image 
+						style={styles.avatar} 
+						source={{ uri: dev.avatar_url }} 
+					/>
+					<Callout onPress={() => {
+						// Navegação para página do GitHub
+						navigation.navigate('Profile', { github_username: dev.github_username })
+					}}>
+						<View style={styles.callout}>
+							<Text style={styles.devName}>{dev.name}</Text>
+							<Text style={styles.devBio}>{dev.bio}</Text>
+							<Text style={styles.devTechs}>{dev.techs.join(', ')}</Text>
+						</View>
+					</Callout>
+				</Marker>
+				})}
+			</MapView>
+			<View style={styles.searchForm}>
+					<TextInput 
+						style={styles.searchInput}
+						placeholder="Buscar devs por tecnologias.."
+						placeholderTextColor="#999"
+						autoCapitalize="words"
+						autoCorrect={false}
+					/>
+					<TouchableOpacity onPress={ loadDevs } style={styles.loadButton}>
+						<MaterialIcons name="my-location" size={20} color="#FFF" />
+					</TouchableOpacity>
+			</View>
+		</>
 	)
 }
 
@@ -70,7 +124,51 @@ const styles = StyleSheet.create({
 		fontSize: 16
 	},
 
+	devBio: {
+		color: '#666',
+		marginTop: 5
+	},
+
 	devTechs: {
 		marginTop: 5
+	},
+
+	searchForm: {
+		position: 'absolute',
+		// bottom: 20,
+		top: 20,
+		left: 20,
+		right: 20,
+		zIndex: 5,
+		flexDirection: 'row'
+	},
+
+	searchInput: {
+		flex: 1,
+		height: 50,
+		backgroundColor: '#FFF',
+		color: '#333',
+		borderRadius: 25,
+		paddingHorizontal: 20,
+		fontSize: 16,
+		shadowColor: '#000',
+		shadowOpacity: 0.2,
+		shadowOffset: {
+			width: 4,
+			height: 4,
+		},
+		elevation: 2
+	},
+
+	loadButton: {
+		width: 50,
+		height: 50,
+		backgroundColor: '#8E4DFF',
+		borderRadius: 25,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginLeft: 15
 	}
+
+
 })
